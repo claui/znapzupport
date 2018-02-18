0a377d98902b24fdf94a6bbf333dd18b506b7359
Claudia <claui@users.noreply.github.com>
2018-02-18 03:53:02 +0100

    Make `znaplizt -m` actually work

--- libexec/znaplizt.bash
+++ libexec/znaplizt.bash
@@ -11,6 +11,7 @@ function __znaplizt {
   local backup_selected=1
   local dataset=
   local dataset_id=
+  local given_dataset_id=
   local verbose=0
   local home_datasets=()
   local home_poolname
@@ -27,7 +28,7 @@ function __znaplizt {
       backup_selected=0
       ;;
     m)
-      dataset_id="${OPTARG}"
+      given_dataset_id="${OPTARG}"
       ;;
     v)
       verbose=1
@@ -71,29 +72,40 @@ function __znaplizt {
   fi
 
   for home_poolname in "${poolnames[@]}"; do
-    if {
-      __dataset_id__load_dataset_record -r "${home_poolname}";
-    }; then
-      if [[ "${home_selected}" -ne 0 ]]; then
-        home_datasets+=("${dataset}")
+    if [[ "${given_dataset_id}" ]]; then
+      if {
+        ! __dataset_id__load_dataset_record -r \
+          -m "${given_dataset_id}" "${home_poolname}"
+      }; then
+        continue
+      fi
+    else
+      if {
+        ! __dataset_id__load_dataset_record -r "${home_poolname}"
+      }; then
+        continue
+      fi
+    fi
+
+    if [[ "${home_selected}" -ne 0 ]]; then
+      home_datasets+=("${dataset}")
+    fi
+
+    if [[ "${backup_selected}" -ne 0 ]]; then
+      if [[ "${verbose}" -ne 0 ]]; then
+        echo >&2 "Looking for backup datasets of ${dataset}"
       fi
 
-      if [[ "${backup_selected}" -ne 0 ]]; then
-        if [[ "${verbose}" -ne 0 ]]; then
-          echo >&2 "Looking for backup datasets of ${dataset}"
+      for backup_poolname in "${poolnames[@]}"; do
+        backup_dataset="${backup_poolname}/backup/${dataset_id}`
+          `/${username}"
+        if {
+          zfs list -H -o name -t filesystem \
+            "${backup_dataset}" >/dev/null 2>&1;
+        }; then
+          backup_datasets+=("${backup_dataset}")
         fi
-
-        for backup_poolname in "${poolnames[@]}"; do
-          backup_dataset="${backup_poolname}/backup/${dataset_id}`
-            `/${username}"
-          if {
-            zfs list -H -o name -t filesystem \
-              "${backup_dataset}" >/dev/null 2>&1;
-          }; then
-            backup_datasets+=("${backup_dataset}")
-          fi
-        done
-      fi
+      done
     fi
   done
 
